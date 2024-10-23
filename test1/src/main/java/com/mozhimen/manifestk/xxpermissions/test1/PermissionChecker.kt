90a8224fe9297dcab468b66b3f04b7dc6133f576
mozhimen <58215586+mozhimen@users.noreply.github.com>
2024-10-23 20:36:05 +0800

    1023

--- test1/src/main/java/com/mozhimen/manifestk/xxpermissions/test1/PermissionChecker.kt
+++ test1/src/main/java/com/mozhimen/manifestk/xxpermissions/test1/PermissionChecker.kt
@@ -64,15 +64,15 @@ object PermissionChecker {
 //    ///////////////////////////////////////////////////////////////////////////
 //
 //    private fun showDialogInstallPermission(context: Context, onGenerate: I_Listener) {
-//        showTipBtnDialog(context, context.getString(com.ty.lelejoy.module_common.R.string.str_permission_install), onGenerate)
+//        showTipBtnDialog(context, context.getString(com.ty.lelejoy.common.R.string.str_permission_install), onGenerate)
 //    }
 //
 //    private fun showDialogReadWritePermission(context: Context, onGenerate: I_Listener) {
-//        showTipBtnDialog(context, context.getString(com.ty.lelejoy.module_common.R.string.str_permission_internal), onGenerate)
+//        showTipBtnDialog(context, context.getString(com.ty.lelejoy.common.R.string.str_permission_internal), onGenerate)
 //    }
 //
 //    private fun showDialogRestart(context: Context, onGenerate: I_Listener) {
-//        showTipBtnNoCloseDialog(context, context.getString(com.ty.lelejoy.module_common.R.string.str_Access_Granted_Please_restart_app_now), onGenerate)
+//        showTipBtnNoCloseDialog(context, context.getString(com.ty.lelejoy.common.R.string.str_Access_Granted_Please_restart_app_now), onGenerate)
 //    }
 //
 //    ///////////////////////////////////////////////////////////////////////////
@@ -81,8 +81,8 @@ object PermissionChecker {
 //        context: Context,
 //        content: String,
 //        block: I_Listener,
-//        title: String = context.getString(com.ty.lelejoy.module_common.R.string.str_Permission_Alert),
-//        btnTxt: String = context.getString(com.ty.lelejoy.module_common.R.string.str_Go_to_Authorize)
+//        title: String = context.getString(com.ty.lelejoy.common.R.string.str_Permission_Alert),
+//        btnTxt: String = context.getString(com.ty.lelejoy.common.R.string.str_Go_to_Authorize)
 //    ) {
 //        DialogTipBtn(context, title, content, btnTxt, block).show()
 //    }
@@ -91,7 +91,7 @@ object PermissionChecker {
 //        context: Context,
 //        content: String,
 //        block: I_Listener,
-//        btnTxt: String = context.getString(com.ty.lelejoy.module_common.R.string.str_Restart_Now)
+//        btnTxt: String = context.getString(com.ty.lelejoy.common.R.string.str_Restart_Now)
 //    ) {
 //        DialogTipBtnNoClose(context, content, btnTxt, block).show()
 //    }
